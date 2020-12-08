697841f894a261fe964a2679ef6233ff80b50888
Myron Byrnes <myronbyrnes@gmail.com>
2020-12-08 14:25:35 -0500

    snakeHead Css and Snake move

--- javascript.js
+++ javascript.js
@@ -5,6 +5,9 @@ const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+const width = 10
+let snakeHead = ""
+
 
 function createGrid() {
     for (let i = 0; i < 100; i++ ) {
@@ -22,12 +25,70 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add("snake"))
 
 function move() {
+    if(
+        (currentSnake[0] + width >= (width * width) && direction === width) || //bottom check
+        (currentSnake[0] - width < 0 && direction === -width) ||
+        (currentSnake[0] % width === (width - 1) && direction === 1) ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    ) {
+        console.log('hit Wall')
+        return  clearInterval(timerId)
+    }
+    
     const tail = currentSnake.pop()
     squares[tail].classList.remove("snake")
-
+    
     currentSnake.unshift(currentSnake[0] + direction)
-    squares[currentSnake[0]].classList.add("snake")
-}
+    
+    squares[currentSnake[0]].classList.add("snake", "snake-head")
+    
+    squares[currentSnake[0]].style.transform = snakeHead
 
+    if(squares[currentSnake[0] - direction].classList.contains('snake-head')) {
+        squares[currentSnake[0] - direction].classList.remove('snake-head')
+    }
+}
 move()
 
+let timerId = (setInterval(move, 1000))
+
+
+function generateApples() {
+    
+}
+/*
+ 39 right
+ 38 up
+ 37 left
+ 40 down
+*/
+
+
+document.addEventListener('keyup', control)
+
+function control(event) {
+    switch (event.keyCode) {
+        case 39:
+            snakeHead = "rotate(0deg)"
+            direction = 1
+            break
+        case 38:
+            snakeHead = "rotate(270deg)"
+            direction =- width
+            break
+        case 37:
+            console.log('left pressed')
+            snakeHead = "rotate(180deg)"
+            direction = -1
+            break
+        case 40:
+            console.log('down pressed')
+            snakeHead = "rotate(90deg)"
+            direction =+ width 
+            break
+        default:
+            break
+    }
+
+}
